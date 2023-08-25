832d80b0d4eb83e3d5044a515f1a79d4c4f176c2
Grzegorz Piekarczyk <grzegorz.piekarczyk@farmaprom.pl>
2023-08-25 07:39:40 +0200

    Dodałem klasę Jądro

---
+++ publiczny/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use ŻółtaGąska\Jądro;
+
+require __DIR__.'/../vendor/autoload.php';
+
+$jądro = new Jądro();
+$jądro->uruchomienieTestowe();
+
---
+++ źródło/Jądro.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ŻółtaGąska;
+
+class Jądro
+{
+    public function __construct()
+    {
+        echo 'Jądro skonstrułowane!!!';
+    }
+
+    public function uruchomienieTestowe(): void
+    {
+        echo 'Jądro::uruchomienieTestowe()';
+    }
+}
+
