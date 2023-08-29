cf158b873a3365bd6d5c25b6bd39505219259659
TheBulaDev <thebuladev@gmail.com>
2023-08-29 13:42:48 +0200

    Aktualizacja skryptu uruchomieniowego, zmiany w Jądro.php

--- publiczny/index.php
+++ publiczny/index.php
@@ -5,5 +5,5 @@ use ŻółtaGąska\Jądro;
 require __DIR__.'/../vendor/autoload.php';
 
 $jądro = new Jądro();
-$jądro->uruchomienieTestowe();
+$jądro->uruchom();
 
--- źródło/Jądro.php
+++ źródło/Jądro.php
@@ -13,14 +13,74 @@ class Jądro
         echo 'Jądro skonstrułowane!!!';
     }
 
-    public function uruchomienieTestowe(): void
+    public function uruchom(): void
     {
         echo 'Jądro::uruchomienieTestowe()';
+        $this->l();
+        echo $this->zwróćGłównyFolder();
+        
+        $this->czytajLink();
+        $this->czytajŚcieżki();
+        $this->generujOdpowiedź();
+    }
+
+    public function pokażDebug(): void
+    {
+        $this->l();
+        echo '$_SERVER';
+        $this->l();
+        var_dump($_SERVER);
+        $this->l();
+        echo '$_SESSION';
+        $this->l();
+        var_dump($_SESSION);
+        $this->l();
+        echo '$_GET';
+        $this->l();
+        var_dump($_GET);
+        $this->l();
+        echo '$_POST';
+        $this->l();
+        var_dump($_POST);
+        $this->l();
+        echo '$GLOBALS';
+        $this->l();
+        var_dump($GLOBALS);
+        $this->l();
+    }
+
+    private function czytajLink(): void
+    {
+    }
+
+    private function generujOdpowiedź(): void
+    {
+    }
+
+    private function zwróćGłównyFolder(): string
+    {
+        return __DIR__ . '/../';
     }
 
     private function czytajŚcieżki(): void
     {
-        $folderŚcieżek = '';
+        $pliki = [];
+        $folderŚcieżek = $this->zwróćGłównyFolder() . 'ścieżki/';
+
+        foreach (scandir($folderŚcieżek) as $zeskanowany) {
+            $scieżkaPliku = $folderŚcieżek . $zeskanowany;
+
+            if (is_file($scieżkaPliku)) {
+                $pliki[] = $zeskanowany;
+            }
+        }
+
+        var_dump($pliki);
+    }
+
+    private function l(): void
+    {
+        echo '<br><br>';
     }
 }
 
